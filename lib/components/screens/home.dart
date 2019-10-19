054bd65d1a0b613a1b6783018a52fddd5f36f307
d0mmi <dommihd@gmail.com>
2019-10-19 10:41:35 +0200

    AA-20 Create Navigation Bar

---
+++ lib/components/screens/home.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return Center(child: Text('Home', style: TextStyle(fontSize: 24.0)));
+  }
+}
---
+++ lib/components/screens/subbox.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SubBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return Center(child: Text('Abos', style: TextStyle(fontSize: 24.0)));
+  }
+}
