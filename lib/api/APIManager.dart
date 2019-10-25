7168bcc59f2f24b71da66ecf9bc67e9ac6c95208
dommi <dommihd@gmail.com>
2019-10-26 01:16:23 +0200

    AA-58 Create Api-Requests
    add News and Calendar Requests

--- lib/api/APIManager.dart
+++ lib/api/APIManager.dart
@@ -1,26 +1,50 @@
 import 'dart:convert';
-import 'dart:developer';
+import 'package:aniflix_app/api/objects/CalendarShow.dart';
 import 'package:aniflix_app/api/objects/Episode.dart';
+import 'package:aniflix_app/api/objects/News.dart';
 import 'package:aniflix_app/api/objects/Show.dart';
 import 'package:aniflix_app/components/screens/home.dart';
 import 'package:aniflix_app/components/slider/SliderElement.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class APIManager {
 
 
-  static Future<List<SliderElement>> getAirings() async {
-    List<SliderElement> airings = [];
-    var response = await _getRequest("show/airing/0");
+  static Future<List<News>> getNews() async {
+    List<News> news = [];
+    var response = await _getRequest("news");
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       var json = jsonDecode(response.body) as List;
-      for(var entry in json){
-        var ep = Episode.fromJson(entry);
-        airings.add(SliderElement(name: ep.season.show.name, description: ep.updated_at,image: "https://www2.aniflix.tv/storage/" + ep.season.show.cover_landscape));
+      for (var entry in json) {
+        news.add(News.fromJson(entry));
       }
     }
+    return news;
+  }
+  static Future<List<CalendarShow>> getCalendarData() async {
+    List<CalendarShow> elements = [];
+    var response = await _getRequest("airing");
+
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.body) as List;
+      for (var entry in json) {
+        elements.add(CalendarShow.fromJson(entry));
+      }
+    }
+    return elements;
+  }
+    static Future<List<SliderElement>> getAirings() async {
+      List<SliderElement> airings = [];
+      var response = await _getRequest("show/airing/0");
+
+      if(response.statusCode == 200){
+        var json = jsonDecode(response.body) as List;
+        for(var entry in json){
+          var ep = Episode.fromJson(entry);
+          airings.add(SliderElement(name: ep.season.show.name, description: ep.updated_at,image: "https://www2.aniflix.tv/storage/" + ep.season.show.cover_landscape));
+        }
+      }
 
     return airings;
   }
