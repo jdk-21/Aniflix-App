b0352a8248d49082ee2b64d3d4f935a738bf774e
Pascal798 <51159141+Pascal798@users.noreply.github.com>
2019-11-07 14:48:25 +0100

    addSubSort

--- lib/components/screens/subbox.dart
+++ lib/components/screens/subbox.dart
@@ -6,9 +6,22 @@ import '../custom/slider/slider_with_headline.dart';
 
 class SubBox extends StatelessWidget {
   Future<List<SubEpisode>> episodes;
+  List<HeadlineSlider> days = [];
+  List<List<SliderElement>> lists = [];
 
   SubBox() {
     this.episodes = APIManager.getSubData();
+  }
+
+  getDays(List<SubEpisode> episodes) {
+    List<DateTime> result = [];
+    for (var episode in episodes) {
+      DateTime d = DateTime.parse(episode.created_at);
+      if(!result.contains(d)){
+        result.add(d);
+      }
+    }
+    return result;
   }
 
   @override
@@ -21,17 +34,49 @@ class SubBox extends StatelessWidget {
             if (snapshot.hasData) {
               List<SubEpisode> episodes = snapshot.data;
               List<SliderElement> elements = [];
-              for(var episode in episodes){
-                var date = DateTime.parse(episode.created_at);
-                //TODO group by Day
-                elements.add(SliderElement(name: episode.show_name + " E"+episode.episode_number.toString()+"S"+episode.season_number.toString(),image: "https://www2.aniflix.tv/storage/"+episode.cover_landscape,));
+
+              List<DateTime> epDays = getDays(episodes);
+
+              for(DateTime day in epDays){
+                List<SliderElement> day = [];
+                lists.add(day);
               }
+
+              for (var i = 0; i <episodes.length; i++) {
+
+                var date = DateTime.parse(episodes[i].created_at);
+                var dateNow = DateTime.now();
+
+                for (var j=0; j<epDays.length; j++) {
+                  var d = epDays[j];
+
+                  if (d.day == date.day && d.month == date.month && d.year == date.year) {
+
+                    SliderElement element = new SliderElement(
+                        name: episodes[i].show_name + " E" +
+                            episodes[i].episode_number.toString() + "S" +
+                            episodes[i].season_number.toString(), image: "https://www2.aniflix.tv/storage/" +
+                            episodes[i].cover_landscape);
+                    lists[j].add(element);
+                    break;
+                  }
+
+                }
+              }
+
+              for (var i = 0; i < lists.length; i++) {
+                if (lists[i].isNotEmpty) {
+                  HeadlineSlider slider = new HeadlineSlider(epDays[i].day.toString() + "."
+                      + epDays[i].month.toString() + "." + epDays[i].year.toString(), ctx, lists[i]);
+                  days.add(slider);
+                }
+              }
+
               return Container(
                   color: Theme.of(ctx).backgroundColor,
                   child: ListView(
-                      padding: EdgeInsets.only(top: 10), children: [
-                        HeadlineSlider("Test", ctx, elements)
-                  ]));
+                      padding: EdgeInsets.only(top: 10),
+                      children: days));
             } else if (snapshot.hasError) {
               return Text("${snapshot.error}");
             }
